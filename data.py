774b557e4b1aa0bd71ecbf2651c8544ba2b719fc
BardiaShir <bardiashirsalimian@gmail.com>
2026-03-24 19:17:14 -0400

    data.py created

---
+++ data.py
@@ -0,0 +1,14 @@
+from datasets import load_dataset
+import pandas as pd
+
+
+def load_sample(n=20):
+    
+    ds = load_dataset("yaful/MAGE" , split="test")
+    df = ds.to_pandas()
+    df = df.drop(columns=["src"])
+#1 : human , 0 : AI
+    sample = df.sample(n=n , random_state= 42).reset_index(drop=True)
+    return sample
+
+print(load_sample())
