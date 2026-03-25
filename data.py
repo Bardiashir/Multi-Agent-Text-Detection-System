b1d07103cff1803c42252002302fec06ec4c720b
BardiaShir <bardiashirsalimian@gmail.com>
2026-03-24 20:46:18 -0400

    Updated run

--- data.py
+++ data.py
@@ -3,11 +3,11 @@ import pandas as pd
 
 
 def load_sample(n=20):
-    
-    ds = load_dataset("yaful/MAGE" , split="test")
+
+    ds = load_dataset("yaful/MAGE", split="test")
     df = ds.to_pandas()
-    df["text"] = df["text"].str.replace("\n", " ")
+    df["text"] = df["text"].str.replace(r"\s+", " ", regex=True).str.strip()
     df = df.drop(columns=["src"])
-#1 : human , 0 : AI
-    sample = df.sample(n=n , random_state= 42).reset_index(drop=True)
+# 1 : human , 0 : AI
+    sample = df.sample(n=n, random_state=42).reset_index(drop=True)
     return sample
--- main.py
+++ main.py
@@ -9,6 +9,9 @@ from sklearn.metrics import classification_report
 from agents import evaluator_a, evaluator_b, evaluator_c, report_generator
 from data import load_sample
 import pandas as pd
+import logging
+
+logging.getLogger("autogen_agentchat").setLevel(logging.WARNING)
 
 load_dotenv()
 
@@ -35,19 +38,19 @@ async def main(text: str):
     agent_report_generator = AssistantAgent(model_client=model_client, name="ReportGenerator", tools=[
         tool_report_generator], system_message=("You are the Report Generator. Use your tool and generate a final verdict. You MUST end your response with exactly 'Final Verdict: HUMAN' or 'Final Verdict: AI'."))
 
-    
     result = await agent_report_generator.run(task=task)
     last_message = result.messages[-1].content
-    if "Final Verdict: HUMAN" in last_message:
+    last_upper = last_message.upper()
+    if "FINAL VERDICT: HUMAN" in last_upper:
         return 1
-    elif "Final Verdict: AI" in last_message:
+    elif "FINAL VERDICT: AI" in last_upper:
         return 0
     else:
-        return -1 
+        return -1
 
 pred_labels = []
 samples = load_sample()
-for i , row in samples.iterrows():
+for i, row in samples.iterrows():
     print(f"Running sample {i + 1}/20.")
     pred = asyncio.run(main(row["text"]))
     pred_labels.append(pred)
@@ -55,10 +58,11 @@ for i , row in samples.iterrows():
 samples["predicted_labels"] = pred_labels
 
 valid = samples[samples["predicted_labels"] != -1]
-accuracy = accuracy_score(valid["label"] ,valid["predicted_labels"])
-print(f"Accuracy is {accuracy *100 :.2f}%")
-print(classification_report(valid["label"], valid["predicted_labels"], target_names=["AI", "HUMAN"]))
-
+accuracy = accuracy_score(valid["label"], valid["predicted_labels"])
+print(f"Accuracy is {accuracy * 100:.2f}%")
+print(classification_report(
+    valid["label"], valid["predicted_labels"], target_names=["AI", "HUMAN"]))
+print(f"Valid samples: {len(valid)}/20")
 
 pd.set_option('display.max_colwidth', 60)
 print("\n===== FULL RESULTS =====")
