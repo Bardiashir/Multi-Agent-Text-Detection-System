0322bfe7b37ec41728f70e18e4b04a198186e0fc
BardiaShir <bardiashirsalimian@gmail.com>
2026-03-26 19:17:37 -0400

    Remove regex expression which was used in older designs

--- data.py
+++ data.py
@@ -5,7 +5,6 @@ def load_sample(n=20):
 
     ds = load_dataset("yaful/MAGE", split="test")
     df = ds.to_pandas()
-    df["text"] = df["text"].str.replace(r"\s+", " ", regex=True).str.strip()
     df = df.drop(columns=["src"])
 # 1 : human , 0 : AI
     sample = df.sample(n=n, random_state=42).reset_index(drop=True)
